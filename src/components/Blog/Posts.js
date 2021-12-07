a9d6e92ea68d9298af6d5584b34604fe396e3044
Tarek Ahmed <tarek5701@gmail.com>
2021-12-07 15:55:36 -0800

    blog card hover effect added

--- src/components/Blog/Posts.js
+++ src/components/Blog/Posts.js
@@ -2,26 +2,27 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Row, Col, Card } from "react-bootstrap";
+import * as styles from "./Posts.module.css";
 
 export default function Posts({ posts }) {
   return (
     <Row className="mt-3">
       {posts.map((post, index) => (
-        <Link href={`/blog/${post.slug}`} key={index}>
-          <Col xs={12} md={6} lg={4} key={post.slug} className="mb-3">
+        <Col xs={12} md={6} lg={4} key={post.slug} className={styles.article}>
+          <Link href={`/blog/${post.slug}`} key={index}>
             <Card>
               <Image src={post.cover_image} height={250} width={700} />
               <Card.Body>
                 <Card.Title>{post.title}</Card.Title>
-                <Card.Text>{post.excerpt}</Card.Text>
+                <Card.Text bsPrefix="small">{post.excerpt}</Card.Text>
+                <div className="d-flex justify-content-between text-muted">
+                  <small className="m-0">{post.date}</small>
+                  <small className="m-0">{post.category}</small>
+                </div>
               </Card.Body>
-              <Card.Footer className="d-flex justify-content-between">
-                <p className="text-muted m-0">{post.date}</p>
-                <p className="text-muted m-0">{post.category}</p>
-              </Card.Footer>
             </Card>
-          </Col>
-        </Link>
+          </Link>
+        </Col>
       ))}
     </Row>
   );
