cf9fee871e66665581495fa9b72583e2c95ef050
Tarek Ahmed <tarek5701@gmail.com>
2021-12-07 20:38:27 -0800

    *

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ function Home() {
     <main>
       <div className="p-3 p-md-5 m-md-3 text-center">
         <div className="col-md-5 mx-auto my-5">
-          <h1 className="display-4 fw-normal">TechSchool</h1>
+          <h1 className="display-4 fw-normal">Toptechschool</h1>
           <p className="lead fw-normal">
             Your one stop website for all technical resources about jobs,
             podcast, blogs, and recent technologies.
